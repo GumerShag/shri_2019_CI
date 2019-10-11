a0cde190b6ba5168d3628d31bca53bff7af0180c
Gumer Shagiyanov <>
2019-10-12 01:32:51 +0300

    Add index for UI

--- server/server.js
+++ server/server.js
@@ -2,25 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const filehandle = require('fs').promises;
 const config = require('./config.server.json');
-const PORT = config["port"] || 3000;
+const PORT = config["port"] || 5000;
 const REPOSITORY_URL = config["repositoryUrl"] || '';
-const AGENTS_APTH = './server/data/agents.json';
+const AGENTS_PATH = './server/data/agents.json';
 
 const app = express();
-
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.notify('/notify_agent', async (req, res) => {
     const {agentHost, agentPort} = req.body;
-    let agents = await filehandle.readFile(AGENTS_APTH);
+    let agents = await filehandle.readFile(AGENTS_PATH);
     agents = JSON.parse(agents.toString());
     //TODO: add validation for unique agent
     agents.push({
         agentHost,
         agentPort
     });
-    await filehandle.writeFile(AGENTS_APTH, JSON.stringify(agents));
+    await filehandle.writeFile(AGENTS_PATH, JSON.stringify(agents));
     await res.sendStatus(200);
 });
 
@@ -36,8 +36,9 @@ app.notify('/notify_build_result', async (req, res) => {
     await res.sendStatus(200);
 });
 
-app.get('/', res => {
+app.get('/', (req, res) => {
     //TODO:Implement UI
+
 });
 app.get('/build/:buildId', (req, res) => {
    //TODO: Implement UI
